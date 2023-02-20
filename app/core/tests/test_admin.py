d555b6a4aa27d3a0d0dc1a6ba06707c81e6f3f44
enoskyeza <enoskyeza@gmail.com>
2023-02-20 10:43:09 +0300

    added test for admin mod

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+"""
+Tests for the django admin modifications.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        """Must be included before tests to create user & client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@exampl.com',
+            password='testpass1234',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_lists(self):
+        """Test users are listed on page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
